76d41ef7b33cdc295be2e6252347c48b6d0ecbff
hyukeh <rnjsgurxo12345@gmail.com>
2024-03-28 17:23:34 +0900

    서블릿

---
+++ servlet/src/main/java/inflearn_srping_mvc1/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package inflearn_srping_mvc1.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private String age;
+
+
+}
