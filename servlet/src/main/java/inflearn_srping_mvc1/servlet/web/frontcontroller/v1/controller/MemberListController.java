1959541c9bcf9abbdd71f064b6342e85f81ba5ef
hyukeh <rnjsgurxo12345@gmail.com>
2024-04-04 15:39:44 +0900

    프론트 컨트롤러 도입 - v1

---
+++ servlet/src/main/java/inflearn_srping_mvc1/servlet/web/frontcontroller/v1/controller/MemberListController.java
@@ -0,0 +1,29 @@
+package inflearn_srping_mvc1.servlet.web.frontcontroller.v1.controller;
+
+import inflearn_srping_mvc1.servlet.domain.member.Member;
+import inflearn_srping_mvc1.servlet.domain.member.MemberRepository;
+import inflearn_srping_mvc1.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListController implements ControllerV1 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+    }
+}
---
+++ servlet/src/main/java/inflearn_srping_mvc1/servlet/web/frontcontroller/v1/controller/MemberSaveController.java
@@ -0,0 +1,32 @@
+package inflearn_srping_mvc1.servlet.web.frontcontroller.v1.controller;
+
+import inflearn_srping_mvc1.servlet.domain.member.Member;
+import inflearn_srping_mvc1.servlet.domain.member.MemberRepository;
+import inflearn_srping_mvc1.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberSaveController implements ControllerV1 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        // Model에 데이터 보관
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
