7622dca950d4590766c940725c8a8792ed0e4769
hyukeh <rnjsgurxo12345@gmail.com>
2024-05-03 17:08:16 +0900

    요청 매핑 헨들러 어뎁터 구조

--- springmvc/src/main/java/inflearn_srping_mvc1/springmvc/basic/response/ResponseBodyController.java
+++ springmvc/src/main/java/inflearn_srping_mvc1/springmvc/basic/response/ResponseBodyController.java
@@ -8,11 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @Slf4j
-@Controller
+@RestController
 public class ResponseBodyController {
 
     @GetMapping("/response-body-string-v1")
